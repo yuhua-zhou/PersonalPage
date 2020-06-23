fb3df346815bfecee60a7b871d86ffd48faa341f
NarrativeTransitions <tangjxhugh@126.com>
2020-06-23 10:42:19 +0800

    Updates

---
+++ precache-manifest.4014285fd80e0ba8604bc889bdcf5bcb.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2225af6494f9091adb5784176decb634",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25483a12409f72fa45d1",
+    "url": "./static/css/2.f11dfeb9.chunk.css"
+  },
+  {
+    "revision": "70dfdb19de4e4455f228",
+    "url": "./static/css/main.030db3d2.chunk.css"
+  },
+  {
+    "revision": "25483a12409f72fa45d1",
+    "url": "./static/js/2.b357c8a1.chunk.js"
+  },
+  {
+    "revision": "70dfdb19de4e4455f228",
+    "url": "./static/js/main.cb8977f0.chunk.js"
+  },
+  {
+    "revision": "c647ce0a396055b7e746",
+    "url": "./static/js/runtime-main.3a6eca83.js"
+  },
+  {
+    "revision": "140480d852f0d717591568e4d7ff11bc",
+    "url": "./static/media/CCPC.140480d8.png"
+  },
+  {
+    "revision": "8118e183a8c4cc205fbaaa9a1adc9234",
+    "url": "./static/media/QQ.8118e183.png"
+  },
+  {
+    "revision": "07bf261ee564731730dddecdf843082d",
+    "url": "./static/media/QQ_QRcode.07bf261e.JPG"
+  },
+  {
+    "revision": "c58a925c2e857b2636f251f06d64fdd0",
+    "url": "./static/media/alipay.c58a925c.png"
+  },
+  {
+    "revision": "cdaca40bf7a0096bf6ac6b3278d56aa3",
+    "url": "./static/media/alipay_QRcode.cdaca40b.PNG"
+  },
+  {
+    "revision": "89e7d8cff9fc2d3126e98cd8712f3ae1",
+    "url": "./static/media/headBg.89e7d8cf.jpg"
+  },
+  {
+    "revision": "44d553782004dce211c32d0e03bc9738",
+    "url": "./static/media/icon.44d55378.jpg"
+  },
+  {
+    "revision": "b3e3cced3cd888e9b0b4aea36633ab8d",
+    "url": "./static/media/logo.b3e3cced.png"
+  },
+  {
+    "revision": "0157a3fe3224c25a25b7235a7593a03a",
+    "url": "./static/media/phone_QRCode.0157a3fe.jpg"
+  },
+  {
+    "revision": "6a24e867ed6e0f1bf01a95b35b9aa728",
+    "url": "./static/media/reward_cup.6a24e867.png"
+  },
+  {
+    "revision": "70a562bb535ec5ca9c63f4e94e1d79cc",
+    "url": "./static/media/robot.70a562bb.png"
+  },
+  {
+    "revision": "70ed2eaabf96f80ddccf5b03de8b4e49",
+    "url": "./static/media/shyz.70ed2eaa.png"
+  },
+  {
+    "revision": "b095c904dc60b7b4af0296f534dbfd79",
+    "url": "./static/media/taobao.b095c904.png"
+  },
+  {
+    "revision": "466427fa9e016426125be2c48e1b4f52",
+    "url": "./static/media/taobao_QRcode.466427fa.PNG"
+  },
+  {
+    "revision": "c942cdc7d719b4f0c9c4359501322e00",
+    "url": "./static/media/weibo.c942cdc7.png"
+  },
+  {
+    "revision": "f30520ad0dc48acacdbbd3bc0c1f6cc6",
+    "url": "./static/media/weibo_QRcode.f30520ad.PNG"
+  },
+  {
+    "revision": "0eb58fe2eb034bb087e2880d923972a1",
+    "url": "./static/media/zju.0eb58fe2.png"
+  },
+  {
+    "revision": "f4960cdd4cc535bb5dec892032a995bb",
+    "url": "./static/media/zstu.f4960cdd.png"
+  },
+  {
+    "revision": "946d0f634b332dd1c80c9f0125854c99",
+    "url": "./static/media/一等奖学金1.946d0f63.png"
+  },
+  {
+    "revision": "e8550e842a9912c4fb4767e09d26a73e",
+    "url": "./static/media/一等奖学金2.e8550e84.png"
+  },
+  {
+    "revision": "a2d3e71e2b47fcdbdf2f868513707dee",
+    "url": "./static/media/三好学生.a2d3e71e.png"
+  },
+  {
+    "revision": "b7849189681a17901e6d7f10b3d652a2",
+    "url": "./static/media/云栖大会.b7849189.png"
+  },
+  {
+    "revision": "43db2ffe7c01ddb565c6ee85257055e1",
+    "url": "./static/media/人工智能创意赛_国二.43db2ffe.jpeg"
+  },
+  {
+    "revision": "8a68935c17ef921e1fc4d701f846aa6c",
+    "url": "./static/media/人工智能创意赛_省二.8a68935c.jpg"
+  },
+  {
+    "revision": "46c565b3d01411bff9f06b28c7a38ec7",
+    "url": "./static/media/国三.46c565b3.jpg"
+  },
+  {
+    "revision": "523b510b785d6fff8dd3b5b0821e82af",
+    "url": "./static/media/国二.523b510b.jpg"
+  },
+  {
+    "revision": "85d699c98146950e902c0c89e91c158b",
+    "url": "./static/media/多媒体.85d699c9.jpg"
+  },
+  {
+    "revision": "5bb60397ea91f0fce74de67111645ea3",
+    "url": "./static/media/接待老校友.5bb60397.png"
+  },
+  {
+    "revision": "41f4f934633c10404e2141660b368296",
+    "url": "./static/media/新苗.41f4f934.png"
+  },
+  {
+    "revision": "9d4332d26f402db5b79b7e69b4cdb832",
+    "url": "./static/media/晚点名.9d4332d2.png"
+  },
+  {
+    "revision": "16d3e55f4fa9367ca5ca33d351d9f15f",
+    "url": "./static/media/校庆志愿者.16d3e55f.png"
+  },
+  {
+    "revision": "8620153bf505d38bb3c5603f1bdeff65",
+    "url": "./static/media/省二.8620153b.jpeg"
+  },
+  {
+    "revision": "0d91e4c3ee045f67164a98184f731bca",
+    "url": "./static/media/省政府1.0d91e4c3.jpg"
+  },
+  {
+    "revision": "29ebe80497fbf4c59af79da238547076",
+    "url": "./static/media/省政府2.29ebe804.jpg"
+  },
+  {
+    "revision": "d44a1af52fbba761d34135a420e6270e",
+    "url": "./static/media/科研宣讲.d44a1af5.png"
+  },
+  {
+    "revision": "a5f2c3d0744518d6b87252c5052492f5",
+    "url": "./static/media/论文.a5f2c3d0.png"
+  },
+  {
+    "revision": "564862dc014eb6845663104ccf018d4d",
+    "url": "./static/media/软测省三.564862dc.jpg"
+  },
+  {
+    "revision": "0c884c5c9666e7af94578c3e16a42597",
+    "url": "./static/media/软著1.0c884c5c.png"
+  },
+  {
+    "revision": "0c96b4d62b24eb03ae570a0bf0ad4a67",
+    "url": "./static/media/软著2.0c96b4d6.png"
+  },
+  {
+    "revision": "b0b72893b0890c4acc8936d1307fe844",
+    "url": "./static/media/软著3.b0b72893.png"
+  },
+  {
+    "revision": "adfed79a57567bacecd75ea19767479f",
+    "url": "./static/media/软著4.adfed79a.png"
+  }
+]);
